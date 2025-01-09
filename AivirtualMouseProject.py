00d2f4820dd9c5ac10c11135f5771f296a836504
Frostyn00 <132602507+Frostyn00@users.noreply.github.com>
2025-01-09 20:25:26 +0300

    Update AivirtualMouseProject.py to V2.0
    Updated and improved version of AIVirtualMouse Project

--- AivirtualMouseProject.py
+++ AivirtualMouseProject.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 import HandTrackingModule as htm
 import time
-import autopy
+import pyautogui
 
 wCam, hCam = 640, 480
-frameR = 100  # Frame Reduction
+frameR = 100
 smoothening = 7
 
 pTime = 0
@@ -16,56 +16,62 @@ cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 detector = htm.handDetector(maxHands=1)
-wScr, hScr = autopy.screen.size()
-# print(wScr, hScr)
+wScr, hScr = pyautogui.size()
+
+clickDone = False
+rightClickDone = False
 
 while True:
-    # 1. Find hand Landmarks
     success, img = cap.read()
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img)
 
-    # 2. Proceed only if landmarks are detected
     if len(lmList) != 0:
-        # Get the tip of the index and middle fingers
-        x1, y1 = lmList[8][1:]
-        x2, y2 = lmList[12][1:]
+        x1, y1 = lmList[8][1:]  # Index finger tip
+        x2, y2 = lmList[12][1:]  # Middle finger tip
+        x3, y3 = lmList[4][1:]   # Thumb tip
+        x4, y4 = lmList[20][1:]  # Pinky tip
 
-        # 3. Check which fingers are up
-        fingers = detector.fingersUp()  # Ensure fingersUp won't throw errors if lmList is empty
-        if len(fingers) >= 2:  # Ensure we have enough data to process fingers
-            # 4. Only Index Finger : Moving Mode
+        fingers = detector.fingersUp()
+
+        if len(fingers) >= 5:
+            if fingers[4] == 1 and not rightClickDone:  # Serçe parmak kalkık
+                pyautogui.rightClick()
+                rightClickDone = True
+
             if fingers[1] == 1 and fingers[2] == 0:
-                # 5. Convert Coordinates
-                x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
-                y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
-                # 6. Smoothen Values
-                clocX = plocX + (x3 - plocX) / smoothening
-                clocY = plocY + (y3 - plocY) / smoothening
-
-                # 7. Move Mouse
-                autopy.mouse.move(wScr - clocX, clocY)
+                x4 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+                y4 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+                clocX = plocX + (x4 - plocX) / smoothening
+                clocY = plocY + (y4 - plocY) / smoothening
+                pyautogui.moveTo(wScr - clocX, clocY)
                 cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
                 plocX, plocY = clocX, clocY
 
-            # 8. Both Index and middle fingers are up : Clicking Mode
             if fingers[1] == 1 and fingers[2] == 1:
-                # 9. Find distance between fingers
                 length, img, lineInfo = detector.findDistance(8, 12, img)
-                print(length)
-                # 10. Click mouse if distance is short
-                if length < 40:
-                    cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
-                    autopy.mouse.click()
+                if length < 20 and not clickDone:
+                    pyautogui.click()
+                    clickDone = True
 
-    # 11. Frame Rate
+            if fingers[1] == 1 and fingers[0] == 1:  # Thumb and Index finger up for scroll
+                length, img, lineInfo = detector.findDistance(8, 4, img)
+                if length <= 100:
+                    pyautogui.scroll(100)
+                elif length > 100:
+                    pyautogui.scroll(-100)
+
+        if fingers[1] == 0 or fingers[2] == 0:
+            clickDone = False
+        if fingers[4] == 0:
+            rightClickDone = False
+
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
     cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
-    # 12. Display
     cv2.imshow("Image", img)
-    
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
